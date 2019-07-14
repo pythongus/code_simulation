76fe1430d3b37bb5c43f6a3f64c47ed86592f036
pythongus@gmail.com <pythongus@gmail.com>
2019-07-15 06:49:44 +1000

    Added factorial

--- factorial.py
+++ factorial.py
@@ -1,5 +1,24 @@
-n = 5
-accum = 1
-for i in range(n, 0, -1):
-    accum *= i
-print(accum)
+#!/usr/bin/python3
+"""Factorial Algorithm
+
+A simple implementation of the classic algorithm.
+"""
+
+def factorial(number):
+    """Calculates the factorial of n"""
+    accum = 1
+    for i in range(number, 0, -1):
+        accum *= i
+
+    return accum
+
+
+def main(number):
+    """Runs the main algorithm and presents the result."""
+    result = factorial(number)
+    print(f"Factorial of {number} is {result}.")
+
+
+if __name__ == '__main__':
+    import sys
+    main(int(sys.argv[1]))
